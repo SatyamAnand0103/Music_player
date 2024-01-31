5162b3e5162e95871e65dfa79e76ee0e27df8db9
satyamanand <satyamanand0103@gmail.com>
2024-01-31 19:31:12 +0530

    updated

--- src/Aside.js
+++ src/Aside.js
@@ -1,9 +1,99 @@
 import React from "react";
 import { musicList_1, musicList_2 } from "./File";
+// import { OneBackward, Oneforward } from "./MasterPlay";
+let a = document.getElementsByClassName("PlayIcon");
+let b = document.getElementsByClassName("myAudios_1");
+let d = document.getElementsByClassName("myAudios_2");
+let e = document.getElementsByClassName("fadeImg");
+let f = document.getElementsByClassName("displaySongBox");
+let g = document.getElementsByClassName("SongDetails");
+
 function Aside() {
+  //Vertical songs list play and pause...
+  let masterPlaybtn = () => {};
   const abc = (i) => {
-    console.log(i+1 + "  song is clicked!");
+    // console.log(i + 1 + " song is selected");
+    for (let j = 0; j < musicList_1.length; j++) {
+      if (i == j) {
+        a[i].src = require("./images/pause_2.png");
+        b[i].load();
+        b[i].play();
+
+        document.getElementById(
+          "masterPlay"
+        ).src = require("./images/pausebtn.png");
+      } else {
+        a[j].src = require("./images/play.png");
+        b[j].pause();
+      }
+    }
+    for (let m = 0; m < musicList_2.length; m++) {
+      e[m].src = require("./images/icon.jpg");
+      d[m].pause();
+    }
+    f[0].style.display = "block";
+    document.getElementById("vibration").style.display = "block";
+    document.getElementById("vibration").src = require("./images/ab.gif");
+
+    f[0].src = musicList_1[i].imgPath;
+    g[0].innerHTML =
+      musicList_1[i].name +
+      "<br>" +
+      musicList_1[i].artist +
+      " , " +
+      " " +
+      musicList_1[i].movie;
   };
+  /////////////////////////////////////////////////////////
+
+  // mouseIn Event On Horizontal music list...
+  let onIn = (j) => {
+    console.log(j + 12 + "hover on ");
+    e[j].style.display = "block";
+  };
+  // mouseOut Event On Horizontal music list...
+
+  let onOut = (j) => {
+    e[j].style.display = "none";
+    //e[j].style.transition = " ease-out 1s";
+
+    console.log(j + 12 + " " + "Left from");
+  };
+  ////////////////////////////////////////////////////////////
+
+  // Horizontal song play and pause...
+  const abcd = (j) => {
+    console.log(j + 12 + " " + " song is selected");
+
+    for (let k = 0; k < musicList_2.length; k++) {
+      if (j == k) {
+        e[j].src = require("./images/icon2.png");
+        d[j].play();
+      } else {
+        e[k].src = require("./images/icon.jpg");
+        d[k].pause();
+      }
+    }
+    for (let m = 0; m < musicList_1.length; m++) {
+      a[m].src = require("./images/play.png");
+      b[m].pause();
+    }
+
+    f[0].style.display = "block";
+    document.getElementById("vibration").style.display = "block";
+    document.getElementById("vibration").src = require("./images/ab.gif");
+
+    f[0].src = musicList_2[j].imgPath;
+    g[0].innerHTML =
+      musicList_2[j].name +
+      "<br>" +
+      musicList_2[j].artist +
+      " , " +
+      " " +
+      musicList_2[j].movie;
+  };
+
+  //vertical song in a playlist//
 
   const listItems_1 = musicList_1.map((eachSong, index) => (
     <div key={index} className="songsItem" onClick={() => abc(index)}>
@@ -18,18 +108,28 @@ function Aside() {
       </div>
       <div>
         <img src={eachSong.playImgPath} className="PlayIcon"></img>
+        <audio className="myAudios_1" src={eachSong.audioPath}></audio>;
       </div>
     </div>
   ));
 
+  // for hiorizontal song playing//
+
   let listItems_2 = musicList_2.map((eachSong, index) => (
-    <div key={index} className="SongsitemInLine">
+    <div
+      key={index}
+      className="SongsitemInLine"
+      onClick={() => abcd(index)}
+      onMouseEnter={() => onIn(index)}
+      onMouseLeave={() => onOut(index)}
+    >
       <img src={eachSong.imgPath} className="songs_img"></img>
       <h5>{eachSong.name} </h5>
       <h6>{eachSong.artist}</h6>
 
       <div className="OnFading">
         <img src={require("./images/icon.jpg")} className="fadeImg"></img>
+        <audio className="myAudios_2" src={eachSong.audioPath}></audio>;
       </div>
     </div>
   ));
@@ -82,9 +182,44 @@ function Aside() {
         </div>
       </div>
 
-      {/* <audio id="myAudios"></audio> */}
+      <div className="bottomBox">
+        <div className="AllIconsOfSongs">
+          <img
+            src={require("./images/backward.png")}
+            id="backwardicon"
+            // onClick={() => OneBackward(index)}
+          ></img>
+          <img
+            src={require("./images/playbtn.png")}
+            className="PlayPauseicon"
+            id="masterPlay"
+            onClick={() => masterPlaybtn()}
+          ></img>
+          <img
+            src={require("./images/forward.png")}
+            id="forwardicon"
+            // onClick={() => Oneforward(index)}
+          ></img>
+        </div>
+
+        <div className="displaySongPoster">
+          <img className="displaySongBox"></img>
+          <div></div>
+          <img id="vibration"></img>
+        </div>
+        <div className="SongDetails"></div>
+        <div className="Range" value="0">
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value="10"
+            step="10"
+            id="ProgressBar"
+          ></input>
+        </div>
+      </div>
     </>
   );
 }
-
 export default Aside;
--- src/MasterPlay.js
+++ src/MasterPlay.js
@@ -1,51 +1,197 @@
-import React from "react";
-// import { MasterPlayOnLoad } from "./File";
-function MasterPlay() {
-  // let callme = () => {
-  //   MasterPlayOnLoad();
-  // };
+let musicList = [
+  {
+    id: "01",
+    audioPath: require("./audios/Tera Hi.mp3"),
+    name: "Tera Hi Bas Hona Chaahoon",
+    artist: "JoJo",
+    movie: "Haunted",
+    imgPath: require("./images/haunted.webp"),
+    playImgPath: require("./images/play.png"),
+    //pauseImgPath: require("./images/pause_2.png"),
+  },
+  {
+    id: "02",
+    audioPath: require("./audios/Sun raha hai na tu.mp3"),
+    name: "Sun Raha Hai Na",
+    artist: "Ankit Tiwari",
+    movie: "Ashique 2",
+    imgPath: require("./images/ashique2.webp"),
+    playImgPath: require("./images/play.png"),
+    //pauseImgPath: require("./images/pause_2.png"),
+  },
+  {
+    id: "03",
+    audioPath: require("./audios/Hmk.mp3"),
+    name: "Hamari Adhuri Kahani",
+    artist: "Arijit Singh",
+    movie: "Hamari Adhuri Kahani",
+    imgPath: require("./images/hak.webp"),
+    playImgPath: require("./images/play.png"),
+    //pauseImgPath: require("./images/pause_2.png"),
+  },
+  {
+    id: "04",
+    audioPath: require("./audios/Jab Bhi Teri.mp3"),
+    name: "Jab Bhi Teri Yaad",
+    artist: "Ishoj",
+    movie: "Album",
+    imgPath: require("./images/jty.webp"),
+    playImgPath: require("./images/play.png"),
+    //pauseImgPath: require("./images/pause_2.png"),
+  },
+  {
+    id: "05",
+    audioPath: require("./audios/Mohabbat.mp3"),
+    name: "Mohabbat Ho Na Jaye",
+    artist: "Kumar Sanu,Alka Yagini",
+    movie: "Kasoor",
+    imgPath: require("./images/kasoor.webp"),
+    playImgPath: require("./images/play.png"),
+    //pauseImgPath: require("./images/pause_2.png"),
+  },
+  {
+    id: "06",
+    audioPath: require("./audios/Kham.mp3"),
+    name: "Khamoshiyaan",
+    artist: "Arijit Singh",
+    movie: "Khamoshiyaan",
+    imgPath: require("./images/kham.webp"),
+    playImgPath: require("./images/play.png"),
+    //pauseImgPath: require("./images/pause_2.png"),
+  },
+  {
+    id: "07",
+    audioPath: require("./audios/Hum.mp3"),
+    name: "Humnava",
+    artist: "Papon",
+    movie: "Hamari Adhuri Kahani",
+    imgPath: require("./images/hh.webp"),
+    playImgPath: require("./images/play.png"),
+    //pauseImgPath: require("./images/pause_2.png"),
+  },
+  {
+    id: "08",
+    audioPath: require("./audios/Naina.mp3"),
+    name: "Naina",
+    artist: "Arijit Singh",
+    movie: "Dangal",
+    imgPath: require("./images/naina.webp"),
+    playImgPath: require("./images/play.png"),
+    //pauseImgPath: require("./images/pause_2.png"),
+  },
+  {
+    id: "09",
+    audioPath: require("./audios/MainDhoondne.mp3"),
+    name: "Mai Dhoondne Ko Zamane Me",
+    artist: "Arijit Singh",
+    movie: "Heartless",
+    imgPath: require("./images/heartless.webp"),
+    playImgPath: require("./images/play.png"),
+    //pauseImgPath: require("./images/pause_2.png"),
+  },
+  {
+    id: "10",
+    audioPath: require("./audios/Pal Pal.mp3"),
+    name: "Pal Pal Dil K Paas",
+    artist: "Kishore Kumar",
+    movie: "BlackMail",
+    imgPath: require("./images/dil k paas.jpg"),
+    playImgPath: require("./images/play.png"),
+    //pauseImgPath: require("./images/pause_2.png"),
+  },
+  {
+    id: "11",
+    audioPath: require("./audios/Milne hai.mp3"),
+    name: "Milne Hai Mujhse Aai",
+    artist: "Arijit Singh",
+    movie: "Ashique 2",
+    imgPath: require("./images/milne hai.jpg"),
+    playImgPath: require("./images/play.png"),
+    //pauseImgPath: require("./images/pause_2.png"),
+  },
+  {
+    id: "12",
+    audioPath: require("./audios/Shiddat.mp3"),
+    name: "Shiddat Bana Lu Tujhe",
+    artist: "Manan Bhardwaj",
+    movie: "Shiddat",
+    imgPath: require("./images/shiddat.jpg"),
+    playImgPath: require("./images/play.png"),
+    //pauseImgPath: require("./images/pause_2.png"),
+  },
+  {
+    id: "13",
+    audioPath: require("./audios/SariDunia.mp3"),
+    name: "Sari Dunia ",
+    artist: "B.Praak",
+    movie: "Animal",
+    imgPath: require("./images/sari dunia.webp"),
+  },
+  {
+    id: "14",
+    audioPath: require("./audios/HareKrishna.mp3"),
+    name: "Hare Krishna ",
+    artist: "Palak Muchhal",
+    movie: "Album",
+    imgPath: require("./images/hk.webp"),
+  },
+  {
+    id: "15",
+    audioPath: require("./audios/OMahi.mp3"),
+    name: "O Mahi ",
+    artist: "Arijit Singh",
+    movie: "Dunki",
+    imgPath: require("./images/Omahi.webp"),
+  },
+  {
+    id: "16",
+    audioPath: require("./audios/SunaSuna.mp3"),
+    name: "Suna Suna",
+    artist: "Shreya Goshal",
+    movie: "Krishna Cottage",
+    imgPath: require("./images/krishnacottage.webp"),
+  },
+  {
+    id: "17",
+    audioPath: require("./audios/SaansonKo.mp3"),
+    name: "Sansoon Ko",
+    artist: "Arijit Singh",
+    movie: "Zid",
+    imgPath: require("./images/zid.webp"),
+  },
 
-  return (
-    <>
-      <div className="bottomBox">
-        <div className="AllIconsOfSongs">
-          <img
-            src={require("./images/backward (3).jpeg")}
-            className="backwardicon" //onClick={OneBackward}
-          ></img>
-          <img
-            src={require("./images/mainplay.png")}
-            className="PlayPauseicon"
-            id="MasterPlay"
-            // onClick={callme}
-          ></img>
-          <img
-            src={require("./images/forward (2).jpeg")}
-            className="forwardicon" //onClick={Oneforward}
-          ></img>
-        </div>
-      
+  {
+    id: "18",
+    audioPath: require("./audios/Bhagwankahan.mp3"),
+    name: "Bhagwan Hai",
+    artist: "Sonu Nigam",
+    movie: "PK",
+    imgPath: require("./images/bhagwan.webp"),
+  },
+  {
+    id: "19",
+    audioPath: require("./audios/Husn.mp3"),
+    name: "Husn",
+    artist: "Anuv Jain",
+    movie: "Album",
+    imgPath: require("./images/husn.webp"),
+  },
+  {
+    id: "20",
+    name: "Kal Ho Na Ho",
+    audioPath: require("./audios/KalHoNaHo.mp3"),
+    artist: "Sonu Nigam",
+    movie: "Kal Ho Na Ho",
+    imgPath: require("./images/kl ho na ho.webp"),
+  },
+  {
+    id: "21",
+    audioPath: require("./audios/Tune.mp3"),
+    name: "Tune Jo Na Kaha",
+    artist: "Mohit Chauhan",
+    movie: "NeW York",
+    imgPath: require("./images/tunejo.webp"),
+  },
+];
 
-      <div className="displaySongPoster">
-        <img src={require("./images/haunted.webp")} id="displaySongBox"></img>
-        <div>
-          <img src={require("./images/vvvv.gif")} id="vibration"></img>
-        </div>
-      </div>
-      <div className="SongDetails"></div>
-      <div className="Range" value="0">
-        <input
-          type="range"
-          min="0"
-          max="100"
-          value="10"
-          step="10"
-          id="ProgressBar"
-        ></input>
-      </div>
-      </div>
-    </>
-  );
-}
-
-export default MasterPlay;
+export { musicList };
